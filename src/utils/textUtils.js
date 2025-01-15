df8d290363c343846f605766ca8dc8cd9101f731
Aryan More <aryanmore498@gmail.com>
2025-01-15 23:51:51 +0530

    Adding navbar in root layout, adding textutils and pnpm, Fixing layout of navbar

---
+++ src/utils/textUtils.js
@@ -0,0 +1,27 @@
+export const navlinks = [
+  {
+    _id: "aa86ba6e-a86a-4c4b-92dd-7f627ed897cd",
+    title: "Products",
+    path: "/#",
+  },
+  {
+    _id: "6de6f5dd-0f0b-4500-87ab-dbd147422062",
+    title: "Get In Touch",
+    path: "/#",
+  },
+  {
+    _id: "4ec61961-dc63-4a4e-9dfc-f45a13affeab",
+    title: "About Us",
+    path: "/#",
+  },
+];
+
+// export const products = [
+//   {
+//     _id: "",
+//     image: "",
+//     product_name: "",
+//     price: "",
+//     btn_text: "Get In Touch",
+//   },
+// ];
