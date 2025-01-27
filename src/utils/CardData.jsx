da23fc6766e74fc3a00c2f8b4e6357bcd8a15f7c
viren7801 <127821519+viren7801@users.noreply.github.com>
2025-01-27 12:56:11 +0530

    added product images

--- src/utils/CardData.jsx
+++ src/utils/CardData.jsx
@@ -1,6 +1,18 @@
 import img1 from "../images/IMG_2642.jpg";
 import img2 from "../images/IMG_2408.jpg";
 import img3 from "../images/IMG_2394.jpg";
+import img4 from "../images/ChocoBlast.png";
+import img5 from "../images/nuttyHearts2.png";
+import img6 from "../images/pomogranateChocolate.png";
+import img7 from "../images/silkyChocolate.png";
+import img8 from "../images/cookieChocolate.png";
+import img9 from "../images/meltyCupcake.png";
+import img10 from "../images/vanillaCupacake.png";
+import img11 from "../images/redvelvetCupcake.png";
+import img12 from "../images/BelgianCupcake.png";
+import img14 from "../images/kitkatCupcake.png";
+import img15 from "../images/oreoCupcake.png";
+import img16 from "../images/strawberryCupcake.png";
 
 export const cardData = [
   {
@@ -24,66 +36,61 @@ export const productsCard = [
   {
     category: "Chocolate",
     title: "Choco Blast",
-    image: "/#",
+    image: img4,
   },
   {
     category: "Chocolate",
     title: "Nutty Hearts",
-    image: "/#",
+    image: img5,
   },
   {
-    category: "Succulent",
+    category: "Chocolate",
     title: "Pomegranate Chocolate",
-    image: "/#",
+    image: img6,
   },
   {
     category: "Chocolate",
     title: "Silky Chocolate",
-    image: "/#",
+    image: img7,
   },
   {
     category: "Chocolate",
-    title: "Milky Oreo",
-    image: "/#",
+    title: "Cookie Chocolate",
+    image: img8,
   },
   {
     category: "Cupcakes",
     title: "Melty Chocolate",
-    image: "/#",
+    image: img9,
   },
   {
     category: "Cupcakes",
     title: "Vanilla Edition",
-    image: "/#",
+    image: img10,
   },
   {
     category: "Cupcakes",
     title: "Red Velvet",
-    image: "/#",
+    image: img11,
   },
   {
     category: "Cupcakes",
     title: "Belgian Chocolate",
-    image: "/#",
+    image: img12,
   },
   {
     category: "Cupcakes",
     title: "Kitkat Cupcake",
-    image: "/#",
+    image: img14,
   },
   {
     category: "Cupcakes",
     title: "Oreo Cupcake",
-    image: "/#",
-  },
-  {
-    category: "Dream Cake",
-    title: "Chocolate",
-    image: "/#",
+    image: img15,
   },
   {
     category: "Dream Cake",
     title: "Strawberry Vanilla",
-    image: "/#",
+    image: img16,
   },
 ];
